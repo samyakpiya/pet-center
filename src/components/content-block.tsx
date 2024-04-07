3e14165d3322e8372925ac64ed303a1df38a32c1
Samyak Piya <samyakpiya@gmail.com>
2024-04-07 13:50:49 -0400

    Refactor account page to include Your Account heading and logged in message

--- src/components/content-block.tsx
+++ src/components/content-block.tsx
@@ -1,6 +1,18 @@
-function ContentBlock({ children }: { children: React.ReactNode }) {
+import { cn } from "@/lib/utils";
+
+type ContentBlockProps = {
+  children: React.ReactNode;
+  className?: string;
+};
+
+function ContentBlock({ children, className }: ContentBlockProps) {
   return (
-    <div className="bg-[#F7F8FA] shadow-sm rounded-md overflow-hidden h-full w-full">
+    <div
+      className={cn(
+        "bg-[#F7F8FA] shadow-sm rounded-md overflow-hidden h-full w-full",
+        className
+      )}
+    >
       {children}
     </div>
   );
