6e7bb94f03ff373e3f77f64faa71846c28d857d1
Samyak Piya <samyakpiya@gmail.com>
2024-04-07 15:59:44 -0400

    Refactor PetButton component to include onClick prop and update PetDetails component

--- src/components/pet-button.tsx
+++ src/components/pet-button.tsx
@@ -4,9 +4,14 @@ import { PlusIcon } from "@radix-ui/react-icons";
 type PetButtonProps = {
   actionType: "add" | "edit" | "checkout";
   children?: React.ReactNode;
+  onClick?: () => void;
 };
 
-export default function PetButton({ actionType, children }: PetButtonProps) {
+export default function PetButton({
+  actionType,
+  onClick,
+  children,
+}: PetButtonProps) {
   if (actionType === "add") {
     return (
       <Button size="icon">
@@ -20,7 +25,11 @@ export default function PetButton({ actionType, children }: PetButtonProps) {
   }
 
   if (actionType === "checkout") {
-    return <Button variant="secondary">{children}</Button>;
+    return (
+      <Button variant="secondary" onClick={onClick}>
+        {children}
+      </Button>
+    );
   }
 
   return <Button></Button>;
--- src/contexts/pet-context-provider.tsx
+++ src/contexts/pet-context-provider.tsx
@@ -14,6 +14,7 @@ type TPetContext = {
   selectedPet: Pet | undefined;
   numberOfPets: number;
   handleChangeSelectedPetId: (id: string) => void;
+  handleCheckoutPet: (id: string) => void;
 };
 
 export const PetContext = createContext<TPetContext | null>(null);
@@ -31,6 +32,11 @@ export default function PetContextProvider({
   const numberOfPets = pets.length;
 
   // event handlers / actions
+  const handleCheckoutPet = (id: string) => {
+    setPets((prev) => prev.filter((pet) => pet.id !== id));
+    setSelectedPetId(null);
+  };
+
   const handleChangeSelectedPetId = (id: string) => {
     setSelectedPetId(id);
   };
@@ -43,6 +49,7 @@ export default function PetContextProvider({
         selectedPet,
         numberOfPets,
         handleChangeSelectedPetId,
+        handleCheckoutPet,
       }}
     >
       {children}
