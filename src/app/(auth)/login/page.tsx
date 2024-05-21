cd22ea41ee84dbfecb7e5cbb8830e1e40303e8b0
Samyak Piya <samyakpiya@gmail.com>
2024-05-20 23:06:03 -0400

    Make auth form reusable by using prop to determine the form's purpose

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -7,7 +7,7 @@ function Page() {
     <main>
       <H1 className="text-center mb-5">Log In</H1>
 
-      <AuthForm />
+      <AuthForm type="logIn"/>
 
       <p className="mt-6 text-sm text-zinc-500">
         No account yet?{" "}
