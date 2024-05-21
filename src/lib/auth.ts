48c41281db11812c96b442a66c8eb28e59019870
Samyak Piya <samyakpiya@gmail.com>
2024-05-21 11:05:11 -0400

    Check user credentials (email and password) with a user from the database. Use Bcrypt to hash passwords.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,13 +1,45 @@
 import NextAuth, { NextAuthConfig } from "next-auth";
+import credentials from "next-auth/providers/credentials";
+import prisma from "./db";
+import bcrypt from "bcrypt";
 
 const config = {
   pages: {
     signIn: "/login",
   },
 
-  providers: [],
+  providers: [
+    credentials({
+      async authorize(credentials) {
+        // runs on login
+        const { email, password } = credentials;
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email,
+          },
+        });
+
+        if (!user) {
+          console.log("User not found");
+          return null;
+        }
+
+        const passwordsMatch = await bcrypt.compare(
+          password,
+          user.hashedPassword
+        );
+
+        if (!passwordsMatch) {
+          console.log("Invalid credentials.");
+          return null;
+        }
+      },
+    }),
+  ],
   callbacks: {
     authorized: ({ request }) => {
+      // runs on every request with middleware
       const isTryingToAccessApp = request.nextUrl.pathname.includes("/app");
 
       if (isTryingToAccessApp) {
