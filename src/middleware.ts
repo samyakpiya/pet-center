db9e712df4dfc3f09e726e0b0cc2fc0f41050d1e
Samyak Piya <samyakpiya@gmail.com>
2024-05-20 23:56:12 -0400

    Specify next-auth config pages, providers, and callbacks

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,6 @@
-import { NextResponse } from "next/server";
+import { auth } from "@/lib/auth";
 
-export function middleware(request: Request) {
-  console.log(request.url);
-  return NextResponse.next();
-}
+export default auth;
 
 export const config = {
   matcher: [
