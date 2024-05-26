e8a0b4919a464ca41a80cd2b3cf99ecfafe4ce4f
Samyak Piya <samyakpiya@gmail.com>
2024-05-26 19:17:39 -0400

    Change token type & finish data fetching

--- src/app/(app)/app/layout.tsx
+++ src/app/(app)/app/layout.tsx
@@ -5,9 +5,21 @@ import { Toaster } from "@/components/ui/sonner";
 import PetContextProvider from "@/contexts/pet-context-provider";
 import SearchContextProvider from "@/contexts/search-context-provider";
 import prisma from "@/lib/db";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
 async function Layout({ children }: { children: React.ReactNode }) {
-  const pets = await prisma.pet.findMany();
+  const session = await auth();
+
+  if (!session?.user) {
+    redirect("/login");
+  }
+
+  const pets = await prisma.pet.findMany({
+    where: {
+      userId: session.user.id,
+    },
+  });
 
   return (
     <>
