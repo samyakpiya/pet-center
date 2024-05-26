78646bd912881977f445e5574e969f3d5829cc98
Samyak Piya <samyakpiya@gmail.com>
2024-05-26 14:25:22 -0400

    Sign Out (Button + Server Action)

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { signIn } from "@/lib/auth";
+import { signIn, signOut } from "@/lib/auth";
 import prisma from "@/lib/db";
 import { sleep } from "@/lib/utils";
 import { petFormSchema, petIdSchema } from "@/lib/validations";
@@ -11,6 +11,12 @@ export async function logIn(formData: FormData) {
   const authData = Object.fromEntries(formData.entries());
 
   await signIn("credentials", authData);
+}
+
+export async function logOut() {
+  await signOut({
+    redirectTo: "/",
+  });
 }
 
 // Pet Actions
