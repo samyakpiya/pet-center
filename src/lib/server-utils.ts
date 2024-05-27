defda668c3875540a01743107702b1fc3bccdfb1
Samyak Piya <samyakpiya@gmail.com>
2024-05-26 21:11:11 -0400

    refactor: fix incorrect parameter name in getPetsByUserId function

--- src/lib/server-utils.ts
+++ src/lib/server-utils.ts
@@ -36,7 +36,7 @@ export async function getPetById(petId: Pet["id"]) {
 export async function getPetsByUserId(userId: User["id"]) {
   const pets = await prisma.pet.findMany({
     where: {
-      id: userId,
+      userId: userId,
     },
   });
 
