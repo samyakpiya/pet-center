a8ad2c9593e315e5fdf6f030cd92cf2a41158b2d
Samyak Piya <samyakpiya@gmail.com>
2024-05-27 22:53:23 -0400

    Add checkout session loading state for server action (useTransition)

--- src/app/(auth)/payment/page.tsx
+++ src/app/(auth)/payment/page.tsx
@@ -4,16 +4,22 @@ import { createCheckoutSession } from "@/actions/actions";
 import H1 from "@/components/h1";
 import { Button } from "@/components/ui/button";
 import { PET_CENTER_LIFETIME_ACCESS_PRICE } from "@/lib/constants";
+import { useTransition } from "react";
 
 export default function Page({ searchParams }) {
+  const [isPending, startTransition] = useTransition();
+
   return (
     <main className="flex flex-col items-center space-y-10">
       <H1>Pet Center access requires payment</H1>
 
       {!searchParams.success && (
         <Button
-          onClick={async () => {
-            await createCheckoutSession();
+          disabled={isPending}
+          onClick={() => {
+            startTransition(async () => {
+              await createCheckoutSession();
+            });
           }}
         >
           Buy lifetime access for {PET_CENTER_LIFETIME_ACCESS_PRICE}
