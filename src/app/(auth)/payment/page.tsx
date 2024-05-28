e4b907d8532d50e147f60dfa61dd32315c508112
Samyak Piya <samyakpiya@gmail.com>
2024-05-27 22:48:56 -0400

    Add searchParams to the URL after payment

--- src/app/(auth)/payment/page.tsx
+++ src/app/(auth)/payment/page.tsx
@@ -5,18 +5,26 @@ import H1 from "@/components/h1";
 import { Button } from "@/components/ui/button";
 import { PET_CENTER_LIFETIME_ACCESS_PRICE } from "@/lib/constants";
 
-export default function Page() {
+export default function Page({ searchParams }) {
   return (
     <main className="flex flex-col items-center space-y-10">
       <H1>Pet Center access requires payment</H1>
 
-      <Button
-        onClick={async () => {
-          await createCheckoutSession();
-        }}
-      >
-        Buy lifetime access for {PET_CENTER_LIFETIME_ACCESS_PRICE}
-      </Button>
+      {!searchParams.success && (
+        <Button
+          onClick={async () => {
+            await createCheckoutSession();
+          }}
+        >
+          Buy lifetime access for {PET_CENTER_LIFETIME_ACCESS_PRICE}
+        </Button>
+      )}
+
+      {searchParams.success && (
+        <p className="text-sm text-green-600">
+          Payment successful! You now have lifetime access to PetCenter.
+        </p>
+      )}
     </main>
   );
 }
