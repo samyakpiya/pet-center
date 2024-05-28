7ba2ce57d5deb503f71f50f7f488fccbd682ab69
Samyak Piya <samyakpiya@gmail.com>
2024-05-27 23:15:56 -0400

    Update redirect rules

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -53,14 +53,19 @@ const config = {
         return false;
       }
 
+      if (isLoggedIn && isTryingToAccessApp && !auth?.user.hasAccess) {
+        return Response.redirect(new URL("/payment", request.nextUrl));
+      }
+
       if (isLoggedIn && isTryingToAccessApp && auth?.user.hasAccess) {
         return true;
       }
 
       if (isLoggedIn && !isTryingToAccessApp) {
         if (
-          request.nextUrl.pathname.includes("/login") ||
-          request.nextUrl.pathname.includes("/signup")
+          (request.nextUrl.pathname.includes("/login") ||
+            request.nextUrl.pathname.includes("/signup")) &&
+          !auth?.user.hasAccess
         ) {
           return Response.redirect(new URL("/payment", request.nextUrl));
         }
