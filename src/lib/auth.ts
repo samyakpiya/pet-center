7a095731365f2de91a0f453bf3c84945d99855d1
Samyak Piya <samyakpiya@gmail.com>
2024-05-31 10:32:07 -0400

    Remove sleep fns to prepare for deployment

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -97,7 +97,6 @@ const config = {
       }
 
       if (trigger === "update") {
-        await sleep(1000);
         // on every request
         const userFromDb = await getUserByEmail(token.email);
         if (userFromDb) {
